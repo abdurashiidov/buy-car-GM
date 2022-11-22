fd6dc29967078f2feeb379e33c5512a22d935bec
abdurashiidov <abdurashiidov18@gmail.com>
2022-11-22 23:29:34 +0200

    commit

---
+++ main.js
@@ -0,0 +1,159 @@
+const list = document.querySelector(".list")
+const rightList = document.querySelector(".rightList")
+const AllSum = document.querySelector(".AllSum")
+// let btn = document.querySelector(".buyBtn")
+
+function getData(param) {
+
+    let value = 0
+    
+    let newArr = []
+    param.map(el => {
+        
+        
+
+        // newArr.push(el)
+        
+        
+        let item = document.createElement("li")
+        let picture = document.createElement("img")
+        let price = document.createElement("p")
+        let buyBtn = document.createElement("button")
+        let title = document.createElement("h2")
+        
+        picture.src = el.url
+        title.textContent = el.name
+        price.textContent = el.price
+        buyBtn.textContent = "Buy"
+        
+        picture.setAttribute("class", "picture")
+        item.setAttribute("class", "li")
+        buyBtn.setAttribute("class", "buyBtn")
+        
+        buyBtn.dataset.id = el.id 
+        
+        item.appendChild(picture)
+        item.appendChild(title)
+        item.appendChild(price)
+        item.appendChild(buyBtn)
+        list.appendChild(item)
+        
+        buyBtn.addEventListener("click", (e) =>{
+            newArr.push(el)
+
+            value = 0;
+            for (let i = 0; i < newArr.length; i++) {
+                value += Number(newArr[i].price)
+                AllSum.textContent = value
+                
+            }
+            
+            console.log(newArr);
+
+            modelData(newArr)
+        })
+        
+    })
+
+    function modelData(params) {
+        rightList.innerHTML = ""
+
+
+
+        params.map(elem => {
+             
+ 
+            // newArr.push(el)
+    
+    
+            let item = document.createElement("li")
+            let picture = document.createElement("img")
+            let price = document.createElement("p")
+            let title = document.createElement("h2")
+            let deleteBtn = document.createElement("button")
+
+            let plus = document.createElement("button")
+            let minus = document.createElement("button")
+    
+            picture.src = elem.url
+            title.textContent = elem.name
+            price.textContent = elem.price
+            deleteBtn.textContent = "x"
+
+            plus.textContent = "+"
+            minus.textContent = "-"
+
+
+            // ! plus btn
+            plus.addEventListener("click", () =>{
+
+                price.textContent = Number(price.textContent) + Number(elem.price)
+
+                value = 0
+
+                value += Number(price.textContent)
+                AllSum.textContent = value
+
+                if (Number(price.textContent) == 0) {
+                    minus.disabled = true
+                } else{
+                    minus.disabled = false
+                }
+
+            })
+
+            // ! minus btn
+            minus.addEventListener("click", () =>{
+                // value = Number(price.textContent) - Number(elem.price)
+                price.textContent = Number(value) - Number(elem.price)
+
+
+                value = Number(price.textContent)
+                AllSum.textContent = value
+
+                if (Number(price.textContent) == 0) {
+                    minus.disabled = true
+                } else{
+                    minus.disabled = false
+                }
+
+            })
+            
+
+
+            picture.setAttribute("class", "picture")
+            item.setAttribute("class", "li")
+            deleteBtn.setAttribute("class", "deleteBtn")
+    
+            deleteBtn.dataset.id = elem.id 
+    
+            // ! del btn
+            deleteBtn.addEventListener("click", (e) =>{
+                newArr =  newArr.filter((par) => par.id != elem.id)
+                
+                modelData(newArr)
+
+                value -= Number(price.textContent)
+                AllSum.textContent = value
+
+            })
+
+            
+            item.appendChild(picture)
+            item.appendChild(title)
+            item.appendChild(price)
+            item.appendChild(deleteBtn)
+
+            item.appendChild(plus)
+            item.appendChild(minus)
+
+            rightList.appendChild(item)
+        })
+    }
+
+
+
+    
+    
+}
+getData(data)
---
+++ new.js
@@ -0,0 +1,27 @@
+let data = [
+    {
+        id: 1,
+        url: "https://s.auto.drom.ru/img5/catalog/photos/fullsize/ravon/nexia_r3/ravon_nexia_r3_294916.jpg",
+        name: "Nexia R3",
+        price: 6000
+    }, 
+    {
+        id: 2,
+        url: "https://www.autostrada.uz/wp-content/uploads/2020/02/chevrolet-lacetti-l-elegant-at-plus-matovo-chernogo-cveta.jpg",
+        name: "Gentra",
+        price: 8000
+    }, 
+    {
+        id: 3,
+        url: "https://s.auto.drom.ru/img1/catalog/photos/fullsize/chevrolet/cobalt/chevrolet_cobalt_34120.jpg",
+        name: "Cobalt",
+        price: 7000
+    }, 
+    {
+        id: 4,
+        url: "https://www.gazeta.uz/media/img/2022/10/ft8n9k16672249744928_l.jpg",
+        name: "Onix",
+        price: 9000
+    }
+
+]
