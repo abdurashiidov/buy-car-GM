c7296c452a758bd9239e30ecfddd97efc17c88cd
abdurashiidov <abdurashiidov18@gmail.com>
2022-12-06 22:07:05 +0200

    add

--- main.js
+++ main.js
@@ -63,8 +63,6 @@ function getData(param) {
              
  
             // newArr.push(el)
-    
-    
             let item = document.createElement("li")
             let picture = document.createElement("img")
             let price = document.createElement("p")
